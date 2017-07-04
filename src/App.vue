ce9355e7f6b8cfaab609d33d869c015379d0d6d3
Luc Princen <luc@chefduweb.nl>
2017-07-04 07:39:34 +0200

    Base project scaffolded

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  	<people></people>
+  </div>
+</template>
+
+<script>
+import People from './components/People.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+  	People
+  }
+}
+</script>
---
+++ src/components/People.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="people-wrapper">
+		<h2>{{ peopleCount }}</h2>
+	</div>
+</template>
+
+
+<script>
+	export default {
+
+		name: 'people',
+		data () {
+			return {}
+		},
+		computed: {
+
+			peopleCount() {
+				return '';
+			}
+
+		}
+	}
+</script>
